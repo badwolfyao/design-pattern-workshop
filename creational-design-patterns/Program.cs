d018ad2598fd3a9ba548be794e61484aa415d5df
Wenyang Yao <wyao@concordservicing.com>
2019-09-30 00:15:09 -0700

    Added Singleton Example

--- creational-design-patterns/Program.cs
+++ creational-design-patterns/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using creational_design_patterns.Singleton;
+using System;
 
 namespace creational_design_patterns
 {
@@ -7,6 +8,29 @@ namespace creational_design_patterns
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            #region Singleton
+
+            ShowSinglton();
+
+            #endregion Singleton
+
+        }
+
+        static void ShowSinglton()
+        {
+            SingletonClass singletonClass1 = SingletonClass.Instance;
+            SingletonClass singletonClass2 = SingletonClass.Instance;
+            if(singletonClass1.ClassID == singletonClass2.ClassID)
+            {
+                Console.WriteLine($"ID for singletonClass1 is {singletonClass1.ClassID}");
+                Console.WriteLine($"ID for singletonClass2 is {singletonClass2.ClassID}");
+                Console.WriteLine("They are identical.");
+            }
+            else
+            {
+                Console.WriteLine("Something goes wrong");
+            }
         }
     }
 }
---
+++ creational-design-patterns/Singleton/SingletonClass.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace creational_design_patterns.Singleton
+{
+    public class SingletonClass
+    {
+        private static SingletonClass _instance = null; // private static instance to be the actual instance
+        private Guid _classID = Guid.Empty; // example field use to compare returned instances
+
+        /// <summary>
+        /// public static instance used to access the private instance
+        /// </summary>
+        public static SingletonClass Instance
+        {
+            get
+            {
+                if(_instance == null)
+                {
+                    _instance = new SingletonClass();
+                }
+                return _instance;
+            }
+        }
+
+        /// <summary>
+        /// Example property used to compare returned instances
+        /// </summary>
+        public Guid ClassID
+        {
+            get
+            {
+                return _classID;
+            }
+            set
+            {
+                _classID = value;
+            }
+        }
+
+        /// <summary>
+        /// Private class constructor, so it can only be access within this class, therefore 
+        /// class will never be able to be created from outside
+        /// </summary>
+        private SingletonClass()
+        {
+            _classID = Guid.NewGuid();
+            Console.WriteLine($"SingletonClass Created with ID={this.ClassID}");
+        }
+    }
+}
