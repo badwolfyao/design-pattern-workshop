ac45dac10e894ef65ade000147906d8ade4966a1
Wenyang Yao <wyao@concordservicing.com>
2019-09-30 00:00:37 -0700

    init projects

---
+++ creational-design-patterns/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace creational_design_patterns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
